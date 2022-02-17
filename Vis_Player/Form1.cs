817137fa859fa21d2da4e9bec391e6857f282c69
VALERIOSPLAY <gate0.public@gmail.com>
2022-02-17 10:56:01 +0300

    git test

--- Vis_Player/Form1.cs
+++ Vis_Player/Form1.cs
@@ -5,7 +5,7 @@ namespace Vis_Player
         public Form1()
         {
             InitializeComponent();
-            Console.WriteLine("Github Test")
+            Console.WriteLine("Github Test");
         }
     }
 }
